f6390c22f50bd7e985ad8212791dd3b7a7a30c7f
Gabriel Baptistella <Bielbapt13@gmail.com>
2022-07-19 18:20:20 -0300

    Gulpfile.js

---
+++ Formulario.php
@@ -0,0 +1,23 @@
+<?php
+	// $nome = "Gabriel";
+	// $sobrenome ="Victor";
+	// $idade = 20;
+
+ // 	$nossoarray = array('nome' => 'Gabriel','idade' => 20,'cidade'=>'São Paulo');
+	// // 	echo"<pre>";
+	// // 		var_dump($nossoarray['cidade']);
+
+	// // echo"seu arquivo php ".$nome." ".$sobrenome;
+	
+	// 	for ($i=0; $i <=10; $i++) {
+	// 		echo "Estamos no indice: ".$i."<br/>";
+	echo"<pre>";
+ var_dump($_POST["texto"]);
+
+			
+		
+
+
+
+
+?>
